ced148745bf5d89ec33e08f7f38dc777520bccef
munjihwan020627 <85243418+munjihwan020627@users.noreply.github.com>
2024-05-18 00:38:31 +0900

    Update items.h

--- SNAKE_GAME/items.h
+++ SNAKE_GAME/items.h
@@ -4,6 +4,9 @@
 class item{
     public:
 
+    int calldefcnt{-1}; // 함수 호출 횟수를 통해 아이템이 몇번 만들어졌는지를 측정함
+    int snakeeatitem{0}; // 추후에 뱀이 아이템을 먹는 알고리즘 구현시 함수호출횟수에서 아이템 유지초수만큼을 빼줄예정
+
     struct grid{
         int r;
         int c;
